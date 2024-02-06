061a3c6d679668c2037927e2446163ee1d8338ea
Utku <utkuhazargel@gmail.com>
2024-02-07 02:20:21 +0300

    css özellikleri verilmesi

--- state/src/App.jsx
+++ state/src/App.jsx
@@ -1,16 +1,15 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Course from "./Course";
 
 function getRandomCourse() {
-  const courseArray = ["Angular", "Bootstrap", "Ccsharp", "KompleWeb"];
-  return courseArray[Math.floor(Math.random() * courseArray.lenght)];
+  const courseArray = ["Angular", "BootStrap5", "CcSharp", "KompleWeb"];
+  return courseArray[Math.floor(Math.random() * courseArray.length)];
 }
 
 function App() {
   const [courses, setCourses] = useState([]);
+
   const handleClick = () => {
     setCourses([...courses, getRandomCourse()]);
   };
@@ -19,9 +18,10 @@ function App() {
   });
   return (
     <div className="App">
-      <button onClick={handleClick}>Kurs Ekle</button>
-     <div>{courseList}</div>
-      <Course />
+      <button className="appButton" onClick={handleClick}>
+        Kurs Ekle
+      </button>
+      <div className="courseList">{courseList}</div>
     </div>
   );
 }
--- state/src/Course.jsx
+++ state/src/Course.jsx
@@ -1,12 +1,23 @@
 import Angular from "./images/angular.jpg";
 import BootStrap5 from "./images/bootstrap5.png";
 import CcSharp from "./images/ccsharp.png";
-import KopmleWeb from "./images/kompleweb.jpg";
+import KompleWeb from "./images/kompleweb.jpg";
+import "./Course.css"
 
-function Course() {
-  return <div> 
-    <img src={Angular} alt="" />
-  </div>;
+const courseMap = {
+  Angular,
+  BootStrap5,
+  CcSharp,
+  KompleWeb,
+};
+function Course({ courseName }) {
+  console.log(courseName);
+  console.log(courseMap[courseName]);
+  return (
+    <div className="courseDiv">
+      <img className="course" src={courseMap[courseName]} alt={courseName} />
+    </div>
+  );
 }
 
 export default Course;
