59533983171ea2694dc4879c0a8b8080e518ae35
Utku <utkuhazargel@gmail.com>
2024-02-07 01:00:44 +0300

    state Fonksiyonu ile rastgele kurs ekleme

---
+++ state/src/Course.jsx
@@ -0,0 +1,12 @@
+import Angular from "./images/angular.jpg";
+import BootStrap5 from "./images/bootstrap5.png";
+import CcSharp from "./images/ccsharp.png";
+import KopmleWeb from "./images/kompleweb.jpg";
+
+function Course() {
+  return <div> 
+    <img src={Angular} alt="" />
+  </div>;
+}
+
+export default Course;
